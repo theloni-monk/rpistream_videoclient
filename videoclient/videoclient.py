801e7c310d7da8888f1430dd89eb51901c8818e9
Theo <theo.acooper@gmail.com>
2019-07-25 13:11:15 -0400

    fixed exit bug

--- videoclient/videoclient.py
+++ videoclient/videoclient.py
@@ -2,13 +2,14 @@ from clientDependency.streamclient import Client
 import socket
 import cv2
 
-SERVER_IP = "YOURIPHERE"
+SERVER_IP = "localhost"
 SERVER_PORT = 5000
 #NOTE: if you change the resolution in the server, you have to change it in the client
 try:
-    client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = True) # Connects to the server
+    client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = False, verbose = True) # Connects to the server
 except socket.error:
     print "Error connecting to duckybot, make sure that your raspberry pi is running your program and you have editted this file to contain the IP address of your duckybot"
+    sys.exit(0)
 client.initializeStream()
 try:
     try:
