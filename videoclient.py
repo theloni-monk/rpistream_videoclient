65cbd4d8fe6964c713168a225f009aa36f79d832
Theo <theo.acooper@gmail.com>
2019-08-03 18:02:42 -0700

    compatability

--- videoclient.py
+++ videoclient.py
@@ -8,16 +8,17 @@ SERVER_IP = "YOURIPHERE"
 SERVER_PORT = 5000
 #NOTE: if you change the resolution in the server, you have to change it in the client
 try:
-    client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = False, verbose = True) # Connects to the server
+    client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = False, verbose = False) # Connects to the server
+    client.s.settimeout(15)
 except socket.error:
-    print "Error connecting to duckybot, make sure that your raspberry pi is running your program and you have editted this file to contain the IP address of your duckybot"
+    print ("Error connecting to duckybot, make sure that your raspberry pi is running your program and you have editted this file to contain the IP address of your duckybot")
     sys.exit(0)
 client.initializeStream()
 try:
     try:
         client.startStream()
     except Exception as e:
-        print e 
+        print(e)
 except KeyboardInterrupt as e:
-    print "exiting"
+    print("exiting")
     cv2.destroyAllWindows()
